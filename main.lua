6e85b9bb6e03f0d2a20ad2a8734e4ed5b7958619
Adrian Kryński <adrian@HP-Compaq-nc6120>
2015-11-24 10:52:23 +0100

    Lol

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ walkTimer = 0.1
 walk = true
 wok = true
 walk1 = 1
-
+isLeft = false
 
 function love.load(arg)
 	hit = love.audio.newSource("Audio/HITMARKER.mp3","static")
@@ -20,73 +20,91 @@ function love.load(arg)
   run2 = love.graphics.newImage("/Gfx/Marian2.png")
   run3 = love.graphics.newImage("/Gfx/Marian3.png")
   jump = love.graphics.newImage("/Gfx/jump.png")
+  stop = love.graphics.newImage("/Gfx/marian_stop.png")
+  runL = love.graphics.newImage("/Gfx/Marian1L.png")
+  run2L = love.graphics.newImage("/Gfx/Marian2L.png")
+  run3L = love.graphics.newImage("/Gfx/Marian3L.png")
+  jumpL = love.graphics.newImage("/Gfx/jumpL.png")
+  stopL = love.graphics.newImage("/Gfx/marian_stopL.png")
   snajpa = love.graphics.newImage("/Gfx/snajpa.png")
   player.img = run
 end
 
 function love.update(dt)
 	if love.keyboard.isDown("escape") then love.event.push("quit") end
-	if love.keyboard.isDown("left","a") then
+  
+  if not love.keyboard.isDown("left", "right", "a", "d") then
+    if isLeft then player.img = stopL
+    else player.img = stop end
+  end
+	
+  if love.keyboard.isDown("left","a") then
 		if player.x > 0 then
 			player.x = player.x - (player.speed*dt)
       if(walk == true) then 
         walk = false
         walkTimer = 0.1
-          if (wok == true) then
-            player.img = run2
-            wok = false
-          else wok = true end
-          if (walk1 == 1) then player.img = run end
-          if (walk1 == 2) then player.img = run2 end
-          if (walk1 == 3) then player.img = run3 end
+        if (walk1 == 1) then player.img = runL end
+        if (walk1 == 2) then player.img = run2L end
+        if (walk1 == 3) then player.img = run3L end
         end
 		end
+    isLeft = true
 	end
+  
 	if love.keyboard.isDown("right","d") then
 		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
 			player.x = player.x + (player.speed*dt)
-         if(walk == true) then 
-          walk = false
-          walkTimer = 0.1
-          if (wok == true) then
-            wok = false
-          else wok = true end
-          if (walk1 == 1) then player.img = run end
-          if (walk1 == 2) then player.img = run2 end
-          if (walk1 == 3) then player.img = run3 end
-        end
+      if(walk == true) then 
+        walk = false
+        walkTimer = 0.1
+        if (walk1 == 1) then player.img = run end
+        if (walk1 == 2) then player.img = run2 end
+        if (walk1 == 3) then player.img = run3 end
+      end
 		end	
+    isLeft = false
 	end
-	if love.keyboard.isDown("down","s") then
+	
+  if love.keyboard.isDown("down","s") then
 		player.y = player.y + (player.speed * dt)
-   	end
-	if love.keyboard.isDown("up","w") then
+  end
+	
+  if love.keyboard.isDown("up","w") then
 		player.y = player.y - (player.speed * dt)
-    player.img = jump
+    if isLeft then player.img = jumpL
+    else player.img = jump end
 	end
-		if love.keyboard.isDown(' ', 'rctrl', 'lctrl', 'ctrl') and canShoot then
-		newBullet = { x = player.x + 150, y = player.y+35, img = bulletImg }
-		table.insert(bullets, newBullet)
+  
+  if love.keyboard.isDown(' ', 'rctrl', 'lctrl', 'ctrl') and canShoot then
+    newBullet = { x = player.x + 150, y = player.y+35, img = bulletImg }
+    table.insert(bullets, newBullet)
     if(player.x > 4) then player.x = player.x-5 end
-		canShoot = false
-		canShootTimer = canShootTimerMax
+    canShoot = false
+    canShootTimer = canShootTimerMax
 	end
+  
   walkTimer = walkTimer - (1 * dt)
-    if walkTimer < 0 then
-      walk = true
-      if (walk1 < 3) then walk1 = walk1 + 1
-      else walk1 = 1 end
-    end
-	canShootTimer = canShootTimer - (1 * dt)
-		if canShootTimer < 0 then
-  		canShoot = true
+  
+  if walkTimer < 0 then
+    walk = true
+    if (walk1 < 3) then walk1 = walk1 + 1
+    else walk1 = 1 end
+  end
+	
+  canShootTimer = canShootTimer - (1 * dt)
+	
+	if canShootTimer < 0 then
+    canShoot = true
 	end
-	for i, bullet in ipairs(bullets) do
-	bullet.x = bullet.x + (500 * dt)
+	
+  for i, bullet in ipairs(bullets) do
+    bullet.x = bullet.x + (500 * dt)
 		if bullet.x < 0 then
 			table.remove(bullets, i)
 		end
 	end
+  
 end
 
 function love.draw(dt)
@@ -95,7 +113,7 @@ function love.draw(dt)
  	love.graphics.draw(snajpa, player.x-13, player.y+20)
  
 	for i, bullet in ipairs(bullets) do
-  		love.graphics.draw(bullet.img, bullet.x, bullet.y)
+    love.graphics.draw(bullet.img, bullet.x, bullet.y)
 	end
   love.graphics.print(tostring(walk1),60,10)
 end
